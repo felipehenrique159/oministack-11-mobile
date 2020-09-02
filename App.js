099f91693632d525915cef7592cf1db803ab8836
felipehenrique159 <felipecod6@hotmail.com>
2020-09-02 13:27:14 -0300

    config de rotas e estrutura

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Meu App react native!</Text>
-      <StatusBar style="auto" />
-      <Button title="Submit"/>
-    </View>
+   <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title:{
-    color: '#fff'
-  }
-
-});
