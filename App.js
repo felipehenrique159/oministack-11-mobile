6b96b77568391ade6e6a54863f7096baa350ccff
felipehenrique159 <felipecod6@hotmail.com>
2020-09-02 12:48:13 -0300

    Arquivos iniciais do projeto

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Meu App react native!</Text>
       <StatusBar style="auto" />
+      <Button title="Submit"/>
     </View>
   );
 }
@@ -14,8 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title:{
+    color: '#fff'
+  }
+
 });
